44e4d9ed8e2be0ec55ce4c6a5f2419684b82127a
Emre B <emreboga@stubhub.com>
2014-04-19 03:19:44 -0700

    Adding library files and bootstrapper JS

--- app/scripts/blue.js
+++ app/scripts/blue.js
@@ -1,4 +1,44 @@
 define([], function() {
     'use strict';
-    alert("I'm here!");
+
+    // Require config allows us to configure shortcut alias
+    require.config({
+        // The shim config allows us to configure dependencies for
+        // scripts that do not call define() to register a module
+        shim: {
+            'dust': {
+                exports: 'dust'
+            },
+            'dust-helpers': {
+                deps: ['dust'],
+                exports: 'dust.helpers'
+            },
+            'underscore': {
+                exports: '_'
+            },
+            'backbone': {
+                deps: ['underscore', 'jquery'],
+                exports: 'Backbone'
+            }
+        },
+        paths: {
+            'backbone': 'lib/backbone',
+            'dust': 'lib/dust-core-2.0.3',
+            'dust-helpers': 'lib/dust-helpers-1.1.2',
+            'jquery': 'lib/jquery',
+            'underscore': 'lib/underscore-1.6.0'
+        }
+    });
+
+    require([
+        'backbone'
+    ], function (Backbone) {
+        // Initialize routing
+
+        // Start Backbone.history()
+        Backbone.history.start();
+
+        // Initialize the application view
+
+    });
 });
